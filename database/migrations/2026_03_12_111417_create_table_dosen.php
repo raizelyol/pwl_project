110291659ed81a591011449c1defd42c4d464b41
raizelyol <fayola.cyrillaedlyn@itbss.ac.id>
2026-04-16 18:27:53 +0700

    Pertemuan 7

---
+++ database/migrations/2026_03_12_111417_create_table_dosen.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('table_dosen', function (Blueprint $table) {
+            $table->id();
+            $table->string('Fullname');
+            $table->string('NIP')->unique();
+            $table->string('NIDN')->unique();
+            $table->string('Pendidikan terakhir');
+            $table->string('Jurusan_id');
+            $table->string('Tempat_Lahir');
+            $table->date('Tanggal_Lahir');
+            $table->text('Alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('table_dosen');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
+Route::get('/mahasiswa-create', [MahasiswaController::class, 'create']);
+Route::post('/mahasiswa', [MahasiswaController::class, 'store'])->name('mahasiswa.save');
+Route::get('/mahasiswa-edit/{id}', [MahasiswaController::class, 'edit']);
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.edit');
